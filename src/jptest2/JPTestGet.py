bf51a4caa07bfec268efc4881766b5ac9032e6c6
Eric Tröbs <1076344+erictroebs@users.noreply.github.com>
2022-09-30 10:01:09 +0200

    2.0.2: add JPTestGet

---
+++ src/jptest2/JPTestGet.py
@@ -0,0 +1,31 @@
+import asyncio
+from os import PathLike
+from typing import Union, List
+
+from .JPTest import JPTest, EXECUTE_TYPE
+from .notebook import Notebook
+
+
+class JPTestGet(JPTest):
+    """
+    decorator to use with test functions
+    """
+
+    def __init__(self, name: str = None, max_score: Union[float, int] = None, timeout: int = 120,
+                 execute: EXECUTE_TYPE = None, get: Union[str, List[str]] = None):
+        super().__init__(name, max_score, timeout, execute)
+        self._get: List[str] = get if isinstance(get, list) else [get]
+
+    async def execute(self, notebook: Union[str, PathLike]):
+        async with Notebook(notebook) as nb:
+            try:
+                if self._execute is not None:
+                    await self._execute_recursively(nb, self._execute)
+
+                references = (nb.ref(name) for name in self._get)
+                values = asyncio.gather(*[ref.receive() for ref in references])
+
+                fun = self._fun(*values)
+                return *(await self._execute_fun(fun)), None
+            except Exception as e:
+                return 0, [str(e)], e
--- src/jptest2/__init__.py
+++ src/jptest2/__init__.py
@@ -1,4 +1,5 @@
 from .JPFixture import JPPreRun, JPPostRun
 from .JPTest import JPTest
 from .JPTestComparison import JPTestComparison
+from .JPTestGet import JPTestGet
 from .notebook import Notebook
